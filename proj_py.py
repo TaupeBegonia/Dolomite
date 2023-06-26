242ffaf4db95cc36762d4dfa45bf206eccb44479
TaupeBegonia <nicole.kuang289@gmail.com>
2023-06-26 14:41:42 -0400

    Removed variable names for form questions

--- proj_py.py
+++ proj_py.py
@@ -3,10 +3,12 @@ import datetime
 import math
 
 # All states that tax period products
-states_tax_dict = {"AL": 0.04, "AZ": 0.056, "AR": 0.065, "GA": 0.04, "HI": 0.04, "ID": 0.06,
-                   "IN": 0.07, "KS": 0.0615, "KY": 0.06, "MS": 0.07, "MO": 0.04225, "NC": 0.0475,
-                   "ND": 0.05, "OK": 0.045, "SC": 0.06, "SD": 0.04, "TN": 0.07, "TX": 0.0625,
-                   "UT": 0.07, "WV": 0.06, "WI": 0.05, "WY": 0.04}
+states_tax_dict = {"AL": 0.04, "AZ": 0.056, "AR": 0.065, "GA": 0.04,
+                   "HI": 0.04, "ID": 0.06, "IN": 0.07, "KS": 0.0615,
+                   "KY": 0.06, "MS": 0.07, "MO": 0.04225, "NC": 0.0475, 
+                   "ND": 0.05, "OK": 0.045, "SC": 0.06, "SD": 0.04, 
+                   "TN": 0.07, "TX": 0.0625, "UT": 0.07, "WV": 0.06, 
+                   "WI": 0.05, "WY": 0.04}
 
 # Create the tkinter GUI
 window = tk.Tk()
@@ -27,6 +29,8 @@ def calculate():
 
     """
     try:
+        
+        # Get all the user inputs
         mens_len = int(a1.get())
         per_len = int(b1.get())
         num_daily = int(c1.get())
@@ -45,6 +49,10 @@ def calculate():
         start_date = datetime.date(int(start_list[0]), int(start_list[1]), int(start_list[2]))
         end_date = datetime.date(int(end_list[0]), int(end_list[1]), int(end_list[2]))
 
+
+        # Start crunching the numbers
+        
+        
         # create a timedelta object
         diff = end_date - start_date
 
@@ -88,24 +96,29 @@ def calculate():
         
         tk.messagebox.showinfo("Calculator Result", sentence)
     
+    
+    # If the user inputs anything wrong (strings for ints, etc)
     except:
         tk.messagebox.showinfo("Error!", "There was an error in you inputs. Try again!")
 
-    
-a = tk.Label(window ,text = "What is your menstrual cycle length in days?").place(x = 100, y = 50)
-b = tk.Label(window ,text = "What is your period length in days?").place(x = 100, y = 75)
-c = tk.Label(window ,text = "How many sanitary products do you use per day?").place(x = 100, y = 100)
-d = tk.Label(window ,text = "How many sanitary products are there per package?").place(x = 100, y = 125)
-e = tk.Label(window ,text = "How much does each package cost?").place(x = 100, y = 150)
 
-f = tk.Label(window ,text = "Enter your start date (year/month/day):").place(x = 100, y = 200)
-g = tk.Label(window ,text = "Enter your end date (year/month/day):").place(x = 100, y = 225)
+# Questions in the form    
+tk.Label(window ,text = "What is your menstrual cycle length in days?").place(x = 100, y = 50)
+tk.Label(window ,text = "What is your period length in days?").place(x = 100, y = 75)
+tk.Label(window ,text = "How many sanitary products do you use per day?").place(x = 100, y = 100)
+tk.Label(window ,text = "How many sanitary products are there per package?").place(x = 100, y = 125)
+tk.Label(window ,text = "How much does each package cost?").place(x = 100, y = 150)
 
-h = tk.Label(window ,text = "What US state do you live in (abbreviation)?").place(x = 100, y = 275)
-h2 = tk.Label(window, text = "(Leave blank if you do not want to include sales tax)").place(x = 100, y = 295)
-i = tk.Label(window ,text = "What is your yearly expected inflation rate (percent)?").place(x = 100, y = 325)
-j = tk.Label(window, text = "(Leave blank if you do not want to include inflation)").place(x = 100, y = 345)
+tk.Label(window ,text = "Enter your start date (year/month/day):").place(x = 100, y = 200)
+tk.Label(window ,text = "Enter your end date (year/month/day):").place(x = 100, y = 225)
 
+tk.Label(window ,text = "What US state do you live in (abbreviation)?").place(x = 100, y = 275)
+tk.Label(window, text = "(Leave blank if you do not want to include sales tax)").place(x = 100, y = 295)
+tk.Label(window ,text = "What is your yearly expected inflation rate (percent)?").place(x = 100, y = 325)
+tk.Label(window, text = "(Leave blank if you do not want to include inflation)").place(x = 100, y = 345)
+
+
+# Textboxes in the form
 a1 = tk.Entry(window)
 a1.place(x = 400, y = 50)
 b1 = tk.Entry(window)
@@ -116,19 +129,20 @@ d1 = tk.Entry(window)
 d1.place(x = 400, y = 125)
 e1 = tk.Entry(window)
 e1.place(x = 400, y = 150)
-
 f1 = tk.Entry(window)
 f1.place(x = 400, y = 200)
 g1 = tk.Entry(window)
 g1.place(x = 400, y = 225)
-
 h1 = tk.Entry(window)
 h1.place(x = 400, y = 275)
 i1 = tk.Entry(window)
 i1.place(x = 400, y = 325)
 
+# The submit button
 tk.Button(window, text="Submit", command = calculate).place(x = 425, y = 350)
 
+
+# Run the application
 window.mainloop()
 
 
